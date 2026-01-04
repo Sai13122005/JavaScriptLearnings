f2100ca3785fc615b022b8f98e0ad99dfff51397
Sai13122005 <pentasaikrishna00@gmail.com>
2026-01-04 16:42:36 +0530

    TODO using DOM in progress

--- TODO/TODO.js
+++ TODO/TODO.js
@@ -1,35 +1,59 @@
-const tasks = [];
-// let req = prompt("Enter your request");
-while(true)
+
+// ___________________Console based_____________________________________________ 
+
+// const tasks = [];
+// // let req = prompt("Enter your request");
+// while(true)
+// {
+//     let req = prompt("Enter your request");
+//     if(req == "quit")
+//     {
+//         alert("Quitting the app");
+//         break;
+//     }
+//     else if(req == "add")
+//     {
+//         let task = prompt("Enter your Task: ");
+//         tasks.push(task);
+//         console.log("Added");
+//     }
+//     else if(req == "list")
+//     {
+//         console.log('------------------');
+//         for(let i = 0; i < tasks.length; i++)
+//         {
+//             console.log(i, tasks[i]);
+//         }
+//         console.log('-------------------');
+//     }
+//     else if(req == 'delete')
+//     {
+//         let idx = prompt("Enter index to delete: ");
+//         tasks.splice(idx, 1);
+//         console.log('Deleted');
+//     }
+//     else{
+//         alert("Invalid request");
+//     }
+// }
+
+
+// ____________________DOM Based______________________________________
+
+let input= document.querySelector("input");
+let btn = document.querySelector(".add");
+let ul = document.querySelector("ul");
+btn.addEventListener("click", function()
 {
-    let req = prompt("Enter your request");
-    if(req == "quit")
+    let li = document.createElement("li");
+    if(input.value != "")
     {
-        alert("Quitting the app");
-        break;
+        li.innerText = input.value;
+        ul.appendChild(li);
+        input.value = "";
     }
-    else if(req == "add")
-    {
-        let task = prompt("Enter your Task: ");
-        tasks.push(task);
-        console.log("Added");
-    }
-    else if(req == "list")
-    {
-        console.log('------------------');
-        for(let i = 0; i < tasks.length; i++)
-        {
-            console.log(i, tasks[i]);
-        }
-        console.log('-------------------');
-    }
-    else if(req == 'delete')
-    {
-        let idx = prompt("Enter index to delete: ");
-        tasks.splice(idx, 1);
-        console.log('Deleted');
-    }
-    else{
-        alert("Invalid request");
-    }
-}
+    let button = document.createElement("button");
+    button.innerText = 'Delete';
+    button.classList.add("delete");
+    li.appendChild(button);
+});
